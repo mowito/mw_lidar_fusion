8c6fce0a68b62d36e82e7e83cea2db1e7bc93809
atharva-18 <atharvapusalkar18@gmail.com>
2021-02-08 18:00:56 +0530

    Add polygon support

--- src/fuse_scan.cpp
+++ src/fuse_scan.cpp
@@ -7,8 +7,88 @@
 #include <laser_geometry/laser_geometry.h>
 #include <vector>
 #include <algorithm>
-#include <math.h>
+#include <cmath>
+#include <limits>
 
+#define EPSI 10e-6
+
+class Point {
+  public:
+  Point(double _x, double _y) {
+      x = _x;
+      y = _y;
+  }
+
+  double x;
+  double y;
+};
+
+bool onSegment(Point p, Point q, Point r) { 
+    if (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) && 
+            q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y)) 
+        return true; 
+    return false; 
+}
+
+int orientation(Point p, Point q, Point r) { 
+    int val = (q.y - p.y) * (r.x - q.x) - 
+            (q.x - p.x) * (r.y - q.y); 
+ 
+    if (val <= EPSI)
+      return 0;
+    return (val > 0)? 1: 2;
+}
+
+bool doIntersect(Point p1, Point q1, Point p2, Point q2) { 
+    int o1 = orientation(p1, q1, p2); 
+    int o2 = orientation(p1, q1, q2); 
+    int o3 = orientation(p2, q2, p1); 
+    int o4 = orientation(p2, q2, q1); 
+ 
+    // General case 
+    if (o1 != o2 && o3 != o4) 
+        return true; 
+ 
+    // Special Cases 
+    // p1, q1 and p2 are colinear and p2 lies on segment p1q1 
+    if (o1 == 0 && onSegment(p1, p2, q1))
+      return true; 
+ 
+    // p1, q1 and p2 are colinear and q2 lies on segment p1q1 
+    if (o2 == 0 && onSegment(p1, q2, q1))
+      return true; 
+ 
+    // p2, q2 and p1 are colinear and p1 lies on segment p2q2 
+    if (o3 == 0 && onSegment(p2, p1, q2))
+      return true; 
+ 
+    // p2, q2 and q1 are colinear and q1 lies on segment p2q2 
+    if (o4 == 0 && onSegment(p2, q1, q2))
+      return true; 
+ 
+    return false;
+} 
+
+bool isInside(std::vector<Point> &polygon, Point p) {
+    if (polygon.size() < 3) return false; 
+ 
+    Point extreme = {std::numeric_limits<double>::max(), p.y}; 
+
+    int count = 0, i = 0; 
+    do { 
+        int next = (i+1)%polygon.size(); 
+
+        if (doIntersect(polygon[i], polygon[next], p, extreme)) {
+            if (orientation(polygon[i], p, polygon[next]) == 0) 
+            return onSegment(polygon[i], p, polygon[next]); 
+ 
+            count++; 
+        } 
+        i = next; 
+    } while (i != 0); 
+ 
+    return count%2;
+}
 
 class FusedScan {
 
@@ -16,6 +96,7 @@ class FusedScan {
         FusedScan(ros::NodeHandle nh) : nh_(nh),
                                         sync_(MySyncPolicy(20), scan_front_, scan_back_)
         {   
+            std::vector<double> polygon_x, polygon_y;
             if (!nh_.param<std::string>("scan_front_topic_name",
                                                 scan_front_topic_name_,
                                                 "/scan/scan_front")) {
@@ -36,6 +117,18 @@ class FusedScan {
             }
             if (!nh_.param("robot_length", robot_length_, 0.65)) {
                 ROS_WARN_STREAM("Did not load robot_length. Standard value is: " << robot_length_);
+            }
+            if (!nh_.param<std::string>("base_link",
+                                                base_link_,
+                                                "base_link")) {
+                ROS_WARN_STREAM("Did not load base_link. Standard value is: " << base_link_);
+            }
+            nh_.getParam("polygon_x", polygon_x);
+            nh_.getParam("polygon_y", polygon_y);
+
+            for (size_t i = 0; i < std::min(polygon_x.size(), polygon_y.size()); i++) {
+                Point p(polygon_x[i], polygon_y[i]);
+                polygon_.push_back(p);
             }
 
             scan_front_.subscribe(nh_, scan_front_topic_name_, 20);
@@ -75,6 +168,9 @@ class FusedScan {
         std::string scan_back_topic_name_;
         std::string fused_scan_topic_name_;
 
+        std::string base_link_;
+        std::vector<Point> polygon_;
+
         double robot_length_;
         double robot_width_;
 
@@ -113,11 +209,11 @@ class FusedScan {
                 scan_fuse.ranges[i]= 40.0;
             }
             for (long int i = 0; i < cloud_fuse.points.size(); i++){
+                Point p(cloud_fuse.points[i].x, cloud_fuse.points[i].y);
+                if (isInside(polygon_, p)) {
+                    continue;
+                }
 
-                if ((cloud_fuse.points[i].x > -robot_length_/2 && cloud_fuse.points[i].x <= robot_length_/2) &&
-                    (cloud_fuse.points[i].y > -robot_width_/2 && cloud_fuse.points[i].y <= robot_width_/2)){
-                        continue;
-                    }
                 float angle = atan2f32(cloud_fuse.points[i].y, cloud_fuse.points[i].x);
                 float range_val = hypotf32(cloud_fuse.points[i].x, cloud_fuse.points[i].y);
                 int index = (int)((angle + M_PIf32)/scan_front->angle_increment);
